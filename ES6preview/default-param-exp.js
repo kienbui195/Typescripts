e0f3e04b300cafad81a592b62df10f07ef0bb13e
Kay Bi <kaybi9x@gmail.com>
2022-07-03 22:06:04 +0700

    Thuc hanh 3

---
+++ ES6preview/default-param-exp.js
@@ -0,0 +1,21 @@
+// function multiply(a,b){
+//     return a*b;
+// }
+
+// function multiply(a,b= 2) {
+//     return a*b;
+// }
+
+class Geeks {
+    constructor(a,b=3) {
+        console.log(a*b);
+    }
+}
+
+// let num1 = multiply(5);
+// console.log(num1);
+// let num2 = multiply(5,8);
+// console.log(num2);
+
+let obj = new Geeks(5);
+let obj1 = new  Geeks(5,4);
