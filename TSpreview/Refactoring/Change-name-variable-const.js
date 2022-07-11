d4fb6f7c1970603ab553941bc817f98ae8e1f6fe
Kay Bi <kaybi9x@gmail.com>
2022-07-11 09:04:59 +0700

    update

---
+++ TSpreview/Refactoring/Change-name-variable-const.js
@@ -0,0 +1,28 @@
+var Calculator = /** @class */ (function () {
+    function Calculator() {
+    }
+    Calculator.calculate = function (firstOperand, secondOperand, operator) {
+        switch (operator) {
+            case this.ADDITION:
+                return firstOperand + secondOperand;
+            case this.SUBTRACTION:
+                return firstOperand - secondOperand;
+            case this.MULTIPLICATION:
+                return firstOperand * secondOperand;
+            case this.DIVISION:
+                if (secondOperand != 0)
+                    return firstOperand / secondOperand;
+                else
+                    console.log("Can not divide by 0");
+            default:
+                console.log("Unsupported operation");
+        }
+    };
+    Calculator.ADDITION = '+';
+    Calculator.SUBTRACTION = '-';
+    Calculator.MULTIPLICATION = '*';
+    Calculator.DIVISION = '/';
+    return Calculator;
+}());
+var result = Calculator.calculate(5, 10, '+');
+console.log(result);
