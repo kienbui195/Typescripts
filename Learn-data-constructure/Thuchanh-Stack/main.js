25a60a81a24a5dfb794b3e4deade0b03df2aa02f
Kay Bi <kaybi9x@gmail.com>
2022-07-14 14:01:24 +0700

    update

---
+++ Learn-data-constructure/Thuchanh-Stack/main.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+var Stack_1 = require("./Stack");
+var stack = new Stack_1.Stack();
+stack.push(5);
+stack.push(5);
+stack.push(5);
+stack.push(5);
+console.log(stack.pop());
---
+++ Learn-data-constructure/Thuchanh-Stack/main.ts
@@ -0,0 +1,9 @@
+import {Stack} from "./Stack";
+
+let stack = new Stack();
+stack.push(5);
+stack.push(5);
+stack.push(5);
+stack.push(5);
+
+console.log(stack.pop());
