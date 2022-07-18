9c6fb3356c2d9d6c3ee6af8d3c5c404cfe27fb42
Kay Bi <kaybi9x@gmail.com>
2022-07-18 15:34:24 +0700

    update

--- Learn-data-constructure/Baitap-MapTree/Lamquen_map/company-info.ts
+++ Learn-data-constructure/Baitap-MapTree/Lamquen_map/company-info.ts
@@ -1,11 +1,30 @@
-let map = new Map();
-map.set('22', 'Infosys');
-map.set(32, 'TCS');
-map.set(false,'Google');
-map.set('45','Facebook');
-map.set('65','Amazon');
-console.log("Employer at index 22: " +map.get('22'));
-console.log("Employer at index 32: " +map.get(32));
-console.log("Size of the map: " +map.size);
-console.log("Deleting a value: " +map.delete('65'));
-console.log( "New Size of the map: " +map.size);
+let nameMap = new Map();
+nameMap.set("Anand",1001);
+nameMap.set("Bhargavi",1002);
+nameMap.set("Chrestin",1003);
+nameMap.set("Daniel",1004);
+nameMap.set("Esther",1005);
+console.log(nameMap.size);
+console.log(nameMap.get("Chrestin"));
+console.log(nameMap.get("Esther"));
+//Iterating map keys
+for (let item of nameMap.keys()) {
+    console.log("Names: "+item);
+}
+//Iterating map values
+for (let item of nameMap.values()) {
+    console.log("ID: ",item);
+}
+//Iterating map entries
+for (let item of nameMap.entries()) {
+    console.log("entries: ", item[0], item[1]);
+}
+Destructuring on object entries
+for (let [key, value] of nameMap) {
+    console.log("key value pairs: ", key, value);
+}
+// item Daniel will get deleted, will return 'true' as output
+nameMap.delete("Daniel")
+// Clear all the entries of the map
+nameMap.clear();
+console.log(nameMap.size);
