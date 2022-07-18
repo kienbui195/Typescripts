3368e4b1e14e2b16cd33ec059111af3a9eae7d36
Kay Bi <kaybi9x@gmail.com>
2022-07-18 17:27:08 +0700

    update

---
+++ Thuattoan/Thuattoan-timkiem/BT-tinhtienphong.js
@@ -0,0 +1,16 @@
+var matrix = [[0, 1, 1, 2], [0, 5, 0, 0], [2, 0, 3, 3]];
+var matrixElementsSum = function (arr) {
+    var sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = 0; j < arr[i].length; j++) {
+            if (i < 2) {
+                if (arr[i][j] == 0) {
+                    arr[i + 1][j] = 0;
+                }
+            }
+            sum += arr[i][j];
+        }
+    }
+    return sum;
+};
+console.log("Tong tien cac phong ngoai tru phong free + phong ngay duoi phong free la: ".concat(matrixElementsSum(matrix)));
