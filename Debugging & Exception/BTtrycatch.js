35e61f20d94c3d5887e5d5b439341f5a74d6a326
Kay Bi <kaybi9x@gmail.com>
2022-07-20 16:52:48 +0700

    update

--- Debugging & Exception/BTtrycatch.js
+++ Debugging & Exception/BTtrycatch.js
@@ -1,27 +1,27 @@
 function calculator(number1, number2, operator) {
-    try {
-        var result = void 0;
-        if (operator != '+' && operator != '-' && operator != '*' && operator != '/') {
-            throw new RangeError('operator must be +,-,*,/');
-        }
-        else {
-            if (operator == '+') {
-                result = number1 + number2;
-            }
-            else if (operator == '-') {
-                result = number1 - number2;
-            }
-            else if (operator == '*') {
-                result = number1 * number2;
-            }
-            else if (operator == '/') {
-                result = number1 / number2;
-            }
-            return result;
-        }
+    var result;
+    if (operator == '+') {
+        result = number1 + number2;
     }
-    catch (e) {
+    else if (operator == '-') {
+        result = number1 - number2;
+    }
+    else if (operator == '*') {
+        result = number1 * number2;
+    }
+    else if (operator == '/') {
+        result = number1 / number2;
+    }
+    if (operator != '+' && operator != '-' && operator != '*' && operator != '/') {
+        throw new RangeError('Error Operator must be +,-,*,/');
+    }
+    return result;
+}
+try {
+    calculator(1, 2, '%');
+}
+catch (e) {
+    if (e instanceof RangeError) {
         console.log(e.message);
     }
 }
-console.log(calculator(1, 2, '0'));
--- Debugging & Exception/BTtrycatch.ts
+++ Debugging & Exception/BTtrycatch.ts
@@ -1,24 +1,26 @@
 function calculator(number1: number, number2: number, operator: string) {
+    let result;
+    if (operator == '+') {
+        result = number1 + number2;
+    } else if (operator == '-') {
+        result = number1 - number2;
+    } else if (operator == '*') {
+        result = number1 * number2;
+    } else if (operator == '/') {
+        result = number1 / number2;
+    }
+    if (operator != '+' && operator != '-' && operator != '*' && operator != '/') {
+        throw new RangeError('Error Operator must be +,-,*,/');
+    }
+    return result;
+}
+
 
     try {
-        let result;
-        if (operator != '+' && operator != '-' && operator != '*' && operator != '/') {
-            throw new RangeError('operator must be +,-,*,/')
-        } else {
-            if (operator == '+') {
-                result = number1 + number2;
-            } else if (operator == '-') {
-                result = number1 - number2;
-            } else if (operator == '*') {
-                result = number1 * number2;
-            } else if (operator == '/') {
-                result = number1 / number2;
-            }
-            return result;
-        }
+        calculator(1,2,'%');
     }
     catch(e){
+        if (e instanceof  RangeError) {
             console.log(e.message)
         }
     }
-    console.log(calculator(1, 2, '0'))
